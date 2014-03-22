3dcb855767ce9ea4fa4134eb836b8fa65aba3cdc
Siming <huyi2218@126.com>
2014-03-22 12:33:44 +0800

    first

---
+++ ESCprogram/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ESCprogram.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ICustomerRepository _repository;
+        public ActionResult Index()
+        {
+            ViewBag.Message = User.Identity.Name;
+            ViewBag.xx = "_repository" ;
+            
+          //  ViewBag.name
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ESCprogram/Models/VendorRecordModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ESCprogram.Models
+{
+    public class VendorRecordModel
+    {
+        public int ID { get; set; }
+        public string topic { get; set; }
+        public int totalthreads { get; set; }
+        public int escthreads { get; set; }
+        public int oobthreads { get; set; }
+        public string alias { get; set; }
+        public DateTime recordtime { get; set; }
+             
+    }
+    public class VendorRecordModelDBContext : DbContext
+    {
+        public DbSet<VendorRecordModel> VendorRecords { get; set; }
+    }
+}
